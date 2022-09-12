2eaed7b867ab11c22f9594160009b0d1f2e01edd
Pacificorp193 <54546134+Pacificorp193@users.noreply.github.com>
2022-09-12 14:59:15 -0600

    Add files via upload

---
+++ www/includes/footer.php
@@ -0,0 +1,10 @@
+    </main>
+
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.6.1.min.js" integrity="sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ=" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.5/jquery.validate.min.js"></script>
+    <script src="/css/jquery.datetimepicker.min.css"></script>
+    <script src="/js/script.js"></script>
+</body>
+</html>
